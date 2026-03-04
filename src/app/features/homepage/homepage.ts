a294fe4779998b0cba770f859b778800a459ed64
StefanoPirastru <stefano.pirastru2001@gmail.com>
2026-03-04 10:31:09 +0100

    Homepage.ts Integrata

--- src/app/features/homepage/homepage.ts
+++ src/app/features/homepage/homepage.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './homepage.html',
-  styleUrl: './homepage.css',
+  styleUrls: ['./homepage.css']
 })
-export class Homepage {
+export class HomepageComponent {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
